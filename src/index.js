0df5fbdba6d4416af9d5a7c92931c48bd89f1d64
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-04 12:55:59 +0000

    feat: add 404 route
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.js
+++ src/index.js
@@ -11,6 +11,12 @@ app.use(
     }),
 );
 
+app.all('*', (_, res) =>
+    res.status(404).json({
+        message: 'The requested resource was not found.',
+    }),
+);
+
 app.listen(parseInt(process.env.PORT) || 3000, '0.0.0.0', () => {
     console.log('Server started');
 });
