fcb1568bd561750f8fd966ee5b22a871da18df36
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-04 12:44:52 +0000

    feat: setup cors, and listen
    Signed-off-by: GitHub <noreply@github.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import 'dotenv/config';
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(
+    cors({
+        origin: '*',
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    }),
+);
+
+app.listen(parseInt(process.env.PORT) || 3000, '0.0.0.0', () => {
+    console.log('Server started');
+});
