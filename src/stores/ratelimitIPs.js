adecb811b3dd9cef2e131c11d66237444f18bc64
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-11 12:34:41 +0000

    feat(stores): add saveEntriesIP function
    Signed-off-by: GitHub <noreply@github.com>

--- src/stores/ratelimitIPs.js
+++ src/stores/ratelimitIPs.js
@@ -1,1 +1,14 @@
-export const ratelimitIPs = new WeakMap();
+export let ratelimitIPs = new Map();
+
+/**
+ * Save the IP address of the client in the ratelimitIPs map.
+ * @param {string} stringEntriesIP string of IPs to add to the ratelimitIPs map
+ * @return {void}
+ */
+export function saveEntriesIP(stringEntriesIP) {
+    ratelimitIPs = new Map([
+        ...ratelimitIPs.entries(),
+        ...Object.entries(JSON.parse(stringEntriesIP)),
+    ]);
+    return;
+}
