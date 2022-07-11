c88869254e8cd5e59df30140e38a1ef12466029d
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-11 12:23:42 +0000

    feat: add redis
    Signed-off-by: GitHub <noreply@github.com>

--- src/redis.js
+++ src/redis.js
@@ -1,10 +1,8 @@
 import Redis from 'ioredis';
 
-/**
- * Create a new Redis client.
- * @return {Redis}
- */
-export const getRedis = () =>
-    new Redis(process.env.REDIS_URL ?? 'redis://:@localhost:6379', {
+export const redis = new Redis(
+    process.env.REDIS_URL ?? 'redis://:@localhost:6379',
+    {
         maxRetriesPerRequest: null,
-    });
+    },
+);
