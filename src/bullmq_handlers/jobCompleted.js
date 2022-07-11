7364f84a464c76c20b25feb8254a0ebb1f3f49d4
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-11 12:30:27 +0000

    feat: add bullmq_handlers
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/bullmq_handlers/jobCompleted.js
@@ -0,0 +1,8 @@
+/**
+ * Job Complete Handler
+ * @param {string} jobId - Job ID
+ * @param {any} value - Job result
+ */
+export const jobCompletedHandler = (jobId, value) => {
+    console.log(jobId, value);
+};
