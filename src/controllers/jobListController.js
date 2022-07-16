ffdf2b1f04e28d705af72d029df9b86b143ae463
Hanif Dwy Putra S <47862061+hansputera@users.noreply.github.com>
2022-07-16 05:53:30 +0000

    feat(controllers): added jobList controller
    Signed-off-by: GitHub <noreply@github.com>

---
+++ src/controllers/jobListController.js
@@ -0,0 +1,25 @@
+/** @typedef {import('express').Request} Request */
+/** @typedef {import('express').Response} Response */
+
+/** @typedef {import('bullmq').Job} Job */
+
+/**
+ * Job List controller
+ * @param {Request} req - Express request object.
+ * @param {Response} res - response object
+ * @return {Promise<void>}
+ */
+export const jobListController = async (req, res) => {
+    /** @type {Job[]} */
+    const jobs = await req.app.settings['bull_queue'].getJobs();
+    return res.status(200).json(
+        jobs.map((j) => ({
+            id: j.data.identifier,
+            status: j.finishedOn
+                ? j.failedReason
+                    ? 'failed'
+                    : 'finished'
+                : 'progress',
+        })),
+    );
+};
